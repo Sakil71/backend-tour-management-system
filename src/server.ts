65453cd3840647102ce6d9c5a19c77fbf310d799
Sakil71 <shakilahamed2171@gmail.com>
2025-07-10 22:15:10 +0600

    project setup

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
-import { error } from "console";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 const startServer = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://tour-management:tour-management110@atlascluster.gbbuibj.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=AtlasCluster"
-    );
+  try {    
+    await mongoose.connect(envVars.DB_URL);
 
     console.log("connected to db");
-    server = app.listen(5000, () => {
-      console.log("Server is listening to port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening on port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
